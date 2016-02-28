f1107739986d5a6f97d1c29a377ed128e968ec57
MattheusNiels <niels.mattheus@student.ucll.be>
2016-02-29 00:08:02 +0100

    Dagelijkse back up datum: ma feb 29 00:08:02 CET 2016  Reason: Test back up script

--- home/centrale/backup_script.py
+++ home/centrale/backup_script.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import os
-
 """Script Author: Niels Mattheus 
    Copyright 2016 Niels Mattheus"""
 
@@ -11,5 +10,6 @@ print("Script Author: Niels Mattheus \n Copyright 2016 Niels Mattheus")
 print ("***Starting to backup system.*** \n")
 reason= raw_input("Reason why you want to backup? ")
 date=os.popen('date -d now').read()
-os.system("cd / && sudo git add /etc && sudo git add /home && sudo git commit -m 'Dagelijkse back up datum: %s Reason: %s' && sudo git push -u origin master" %(date,reason))
-print ("***Backup is completed!*** \n")
+os.system("cd / && sudo git add /etc && sudo git add /home && sudo git add /root && sudo git commit -m 'Dagelijkse back up datum: %s Reason: %s' && sudo git push -u origin master" %(date,reason))
+print ("***Backup is completed*** \n")
+
